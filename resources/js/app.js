edb653e01e6dfd33951de2c36b294db3199bddc6
EM Cabua <bsitcabua@gmail.com>
2020-04-25 17:14:44 +0800

    Created main app vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+// Load the vue lib
+window.Vue = require('vue');
+
+// Load the main app component
+Vue.component('mainapp', require('./components/mainapp.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/welcome', 'WelcomeController@index')->name('welcome');
+
+Route::any('{slug}', function(){
+    return view('index');
 });
